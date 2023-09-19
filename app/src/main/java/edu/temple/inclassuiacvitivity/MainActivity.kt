1b30128bf7c90a9507e1c78a40e1144dd164c0c9
aliasgersg <alighotawala02@gmail.com>
2023-09-19 12:01:46 -0400

    Created Adapter to display items from array in Spinner and also changed the TextView's text size to the number selected in the Spinner

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -14,15 +14,40 @@ class MainActivity : AppCompatActivity() {
         val displayTextView = findViewById<TextView>(R.id.textDisplay)
 
         /* Step 1: Populate this array */
-        val numberArray = IntArray(20) {it * 5}
+        val numberArray = Array(20) {it * 5}
 
 
         /* Step 2: Create adapter to display items from array in Spinner */
-        //spinner.adapter = ArrayAdapter...
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, numberArray)
+        spinner.adapter = adapter
+
 
 
         // Step 3: Change TextView's text size to the number selected in the Spinner */
-        //spinner.onItemSelectedListener = object: ...
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                // Retrieve the selected item
+                val selectedItem = parent?.getItemAtPosition(position)
+
+                // Update the TextView with the selected item
+                displayTextView.text = "Air Supply is the best band EVER!"
+
+                // Change the text size of the TextView based on the selected number
+                if (selectedItem is Int) {
+                    // Convert the selected number to a float for text size
+                    val textSize = selectedItem.toFloat()
+
+                    // Set the text size of the TextView
+                    displayTextView.textSize = textSize
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Handle the case where nothing is selected (if needed)
+            }
+        }
+
+
 
     }
 }
